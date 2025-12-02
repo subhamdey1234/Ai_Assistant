b1f50ee7f4ed0391678e7f7f4cd26eaab65b8e14
Subham Dey <74315357+subhamdey1234@users.noreply.github.com>
2025-12-03 00:08:39 +0530

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import geminiResponse from "./gemini.js";
 const app = express();
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://myvirtualassistant11.netlify.app',
     credentials:true,
 }))
 
